ddc514b25459852e44e4aaffd2c933afba239bfe
Богдан Чепа <bodik_kz00@ukr.net>
2022-03-22 00:04:09 +0200

    added sample

---
+++ samples/B9_Pagination.Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using B9_Pagination;
+using B9_Pagination.Sample;
+using MockQueryable.Moq;
+
+var range = Enumerable.Range(1,99)
+    .Select(x => new User(x));
+
+var items = range.AsQueryable().BuildMockDbSet().Object;
+
+var pagination = new PaginationQuery(pageNumber: 1, pageSize: 50);
+
+var paginationResult = await items
+    .Where(x => x.Id > 30)
+    .GetPaginationAsync(pagination);
+
+var serializeResult = JsonSerializer.Serialize(paginationResult);
+Console.WriteLine(serializeResult);
---
+++ samples/B9_Pagination.Sample/User.cs
@@ -0,0 +1,9 @@
+﻿namespace B9_Pagination.Sample;
+
+public class User
+{
+    public User(int id)
+        => Id = id;
+    
+    public int Id { get; set; }
+}
